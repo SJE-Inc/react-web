6dc630e27edb17cc226886680de5fa81f38dc33d
SJE-inc <contacts.sje@gmail.com>
2022-08-01 12:02:57 -0400

    messenger fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages';
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
 
@@ -9,6 +9,10 @@ function App() {
   return (
     <Router>
       <Home />
+      <MessengerCustomerChat
+        pageId="102430202572038"
+        appId="925574071736483"
+      />
     </Router>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ import Footer from '../components/Footer'
 import PageLoader from '../components/PageLoader/PageLoader'
 
 
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
 
@@ -33,10 +32,6 @@ const Home = () => {
             <InfoSection {...homeObjTwo} />
             <UserForm />
             <Footer />
-            <MessengerCustomerChat
-                pageId="102430202572038"
-                appId="925574071736483"
-            />
         </>
     )
 }
